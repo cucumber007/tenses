de0a1de55c9550e5ed9b5599a393ef7a2788616c
pavel-kuzntesov-5x <pavel.kuzntesov.5x@gmail.com>
2021-04-11 13:29:54 +0300

    Changes

--- make exe.bat
+++ make exe.bat
@@ -1,1 +1,2 @@
-C:\Users\cucumber007\AppData\Roaming\Python\Python39\Scripts\pyinstaller --onefile tenses.py
+#C:\Users\cucumber007\AppData\Roaming\Python\Python39\Scripts\
+pyinstaller --onefile tenses.py
--- tenses.py
+++ tenses.py
@@ -1,39 +1,34 @@
+import json
 import random
 
-i = ["я", "ты", "мы", "вы", "они", "он", "она", "оно"]
-will = ["буду", "будешь", "будем", "будете", "будут", "будет"]
-future = ["буду", "будете", "будет"]
-words = [
-    [
-        {
-            "я,он,ты": "ходил",
-            "мы,вы,они": "ходили",
-            "она": "ходила",
-            "оно": "ходило",
-        },
-        ["хожу", "ходишь", "ходим", "ходите", "ходят", "ходит"],
-        ["ходить"],
-    ],
-    [
-        {
-            "я,он,ты": "летал",
-            "мы,вы,они": "летали",
-            "она": "летала",
-            "оно": "летало",
-        },
-        ["летаю", "летаешь", "летаем", "летаете", "летают", "летает"],
-        ["летать"],
-    ],
-]
+sample_data = json.loads('{"gender_male":true,"i"'
+                         ':["я","ты","мы","вы","они","он","она","оно"],"will":["буду","будешь","будем","будете","будут","будет"],"future":["буду","будете","будет"],"words":[[{"мы,вы,они":"ходили","она":"ходила","он":"ходил","оно":"ходило"},["хожу","ходишь","ходим","ходите","ходят","ходит"],["ходить"]],[{"я,он,ты":"летал","мы,вы,они":"летали","она":"летала","оно":"летало"},["летаю","летаешь","летаем","летаете","летают","летает"],["летать"]]]}')
+
+try:
+    with open("data.json", "r", encoding="utf-8") as f:
+        data = json.loads(f.read())
+except FileNotFoundError as e:
+    print("data.json not found, creating form sample...\n")
+    data = sample_data
+    with open("data.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(data, indent=4, ensure_ascii=False))
+        f.close()
+
+i = data["i"]
+will = data["will"]
+future = data["future"]
+words = data["words"]
+gender_male = data["gender_male"]
 
 
 def gen():
     res = ""
     iv = random.choice(i)
+    # iv = random.choice(["я", "ты"])
     inn = i.index(iv)
     res += str(iv)
     tense = random.randint(0, 2)
-    tense = 0
+    # tense = 0
     w = random.choice(words)[tense]
 
     nnot = bool(random.randint(0,1))
@@ -53,6 +48,15 @@ def gen():
         for k, v in w.items():
             if iv in k.split(","):
                 wz = v
+        if wz is None:
+            if iv == "я":
+                if gender_male:
+                    wz = w["он"]
+                else:
+                    wz = w["она"]
+            elif iv ==  "ты":
+                wz = w[random.choice(["он", "она"])]
+
 
         res += f"{n}{wz}{qv}"
     if tense == 1:  # present
@@ -78,3 +82,4 @@ while True:
     last = g
     print(g)
     input()
+
